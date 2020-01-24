ff7e7814c1dc7587cdb0f20be085d8848715e8d7
Eric Bizet <eric.bizet@etu.univ-nantes.fr>
2020-01-24 14:41:57 +0100

    Consommation queue up, paths

--- app/consumer.py
+++ app/consumer.py
@@ -8,13 +8,9 @@ class Consumer:
         self.feeder = feed_hyperplan
         self.q = persistqueue.SQLiteQueue(os.path.join(os.path.dirname(__file__), abs_path('queue')), auto_commit=True)
 
-        # self.writer = StoreResult()
-        # self.writer.connect(os.path.join(os.path.dirname(__file__), "classification_db/offer_classification.db"))
-
 
     def consume(self):
         print("En attente d'offres...")
         while True:
             offer_data = self.q.get()
-            print(offer_data)
-            # self.feeder.classify(offer_data["id"], offer_data["clean_text"])
+            self.feeder.classify(offer_data["id"], offer_data["clean_text"])
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from feed_hyperplan import FeedHyperplan
 from consumer import Consumer
 
 feeder = FeedHyperplan()
-feeder.classify(0, "machine")
+# feeder.classify(0, "machine")
 
-# consumer = Consumer(feeder)
-# consumer.consume()
+consumer = Consumer(feeder)
+consumer.consume()
