f346390d544f19da46dffd1dffc0ef190f443a9b
Eric Bizet <eric.bizet@etu.univ-nantes.fr>
2020-03-01 12:03:52 +0100

    Intégration mnb sklearn

--- app/producers/producer_test.py
+++ app/producers/producer_test.py
@@ -2,6 +2,6 @@ import persistqueue
 import os
 
 q = persistqueue.SQLiteQueue(os.path.join(os.path.dirname(__file__), "../queue"), auto_commit=True)
-q.put({"id": 0, "clean_text": "machine développeur"})
-q.put({"id": 1, "clean_text": "développeur"})
-q.put({"id": 2, "clean_text": "image"})
+q.put({"id": 0, "clean_text": "intelligence artificielle"})
+q.put({"id": 1, "clean_text": "v2x"})
+q.put({"id": 2, "clean_text": "developpeur web api"})
