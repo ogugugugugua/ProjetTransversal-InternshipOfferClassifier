7d254de9dc8c236f06e2421c88a9b3f660442bd5
Yulin XIE <yulin.xie@etu.univ-nantes.fr>
2020-05-10 18:36:18 +0200

    Update FeedHyperplan.py

--- app/producers/FeedHyperplan.py
+++ app/producers/FeedHyperplan.py
@@ -1,15 +1,23 @@
 import os
 import time
 import persistqueue
+import random
 
 class FeedHyperplan:
+
+    def __init__(self):
+        self.list = ["intelligence artificielle", "v2x", "developpeur web api",
+            "developpeur stm32", "ingenieur architecture web",
+            "Technicien informatique H/F", "technicien informatique d’exploitation INDRET (H/F)",
+            "Testeur Informatique", "Developpeur web full stack H/F",
+            "Collaborateur comptable H/F", "Technicien(ne) maintenance informatique",
+            "Developpeur PHP Back Office", "Developpeur Front-end Angular/NodeJS/Javascript",
+            "Tech lead Full Stack Java", "Stage Developpeur Web Javascript"]
 
     def feed(self):
         q = persistqueue.SQLiteQueue(os.path.join(os.path.dirname(__file__), "../queue"), auto_commit=True)
         for i in range(100):
-            q.put({"id": 0, "clean_text": "intelligence artificielle"})
-            q.put({"id": 1, "clean_text": "v2x"})
-            q.put({"id": 2, "clean_text": "developpeur web api"})
+            q.put({"id": i, "clean_text": random.choice(self.list)})
 
     def MesureTemps(self):
         startTime = time.time()
